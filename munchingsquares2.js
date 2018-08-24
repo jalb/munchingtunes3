f499d948f0226953093f1f6ea2eb04b6dac83f7d
jalb <jalb@pobox.com>
2018-08-24 19:56:45 +0200

    Slow it down.

--- munchingsquares2.js
+++ munchingsquares2.js
@@ -199,7 +199,7 @@ function StartMunchingSquares(seed) {
     number= 0;
     if (!timerActive) {
 	    timerActive= true;
-	    timer = setInterval(Animate, 1);
+	    timer = setInterval(Animate, 10);
 	}
 }
 
